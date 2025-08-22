3833ddce50085900f728ea259169e3b7575dff01
martin reda <martinreda999@outlook.com>
2025-08-22 23:10:48 +0300

    01 Non-Generic Collection [HashTable]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Demo
+﻿using System.Collections;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Non-Generic Collection [HashTable]
+            /*
+             * Hash Table => Key value pair 
+             * -Key must be unique
+             * -Key cannot be null
+             * -value Can Be null 
+             * 
+             */
+            Hashtable PhoneNote = new Hashtable()
+            {
+                //{"pola", 123 },
+                //{"dd", 147 },
+                //{"ee", 125 }
+
+                 //Indexer 
+
+                ["mona"]=123,
+                ["moeena"]=47,
+                ["mosssna"]=41
+            };
+
+           
+
+
+
+            //phone note of type hashtable has size 3
+
+            //Key EMp name 
+            //Value Phone number
+            //to add elements
+
+            //PhoneNote.Add("martin", 123);
+            //PhoneNote.Add("reda", 147);
+            //PhoneNote.Add("faiz", 125);
+
+            //foreach (object item in PhoneNote)
+            //    Console.WriteLine(item);
+            foreach (object item in PhoneNote)
+                Console.WriteLine(item);
+
+            //print spisfaiy data => Dictionary entry 
+
+            #endregion
         }
     }
 }
